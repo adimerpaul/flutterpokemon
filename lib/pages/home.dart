54494b001fdfb5410ea2b9d43318e6c2f852e760
adimer <adimer101@gmail.com>
2024-10-17 21:53:14 -0400

    Actulizado par aistatas dinamica

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -8,19 +8,44 @@ class MyHome extends StatefulWidget {
 }
 
 class _MyHomeState extends State<MyHome> {
+  List<String> items = ['Item 1', 'Item 2', 'Item 3'];
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home'),
       ),
-      body: ListView(
+      body: Column(
         children: [
-          Text('Item A'),
-          Text('Item B'),
-          Text('Item Z'),
+          ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  items.add('Item ${items.length + 1}');
+                });
+              },
+              child: const Text('Add Item')
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: items.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(items[index]),
+                );
+              },
+            ),
+          )
         ],
-      )
+      ),
+      // ListView(
+      //   children: [
+      //     Text('Item A'),
+      //     Text('Item B'),
+      //     Text('Item Z'),
+      //   ],
+      // )
     );
   }
 }
